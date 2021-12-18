8a5ea69485e25ff9e767252fd4aefaa28bbe88e7
Jan9669 <82373118+Jan9669@users.noreply.github.com>
2021-12-18 12:00:19 +0100

    Add files via upload

---
+++ Frontend/plantcaretable.js
@@ -0,0 +1,34 @@
+const formEl = document.querySelector("form");
+      const tbodyEl = document.querySelector("tbody");
+      const tableEl = document.querySelector("table");
+      
+      function onAddtask(e) {
+        e.preventDefault();
+        const plantname = document.getElementById("plantname").value;
+        const assigneept = document.getElementById("assigneept").value;
+        const howtocare = document.getElementById("howtocare").value;
+        const description = document.getElementById("description").value;
+        
+        tbodyEl.innerHTML += `
+            <tr>
+                <td>${plantname}</td>
+                <td>${assigneept}</td>
+                <td>${howtocare}</td>
+                <td>${description}</td>
+                
+                <td><button class="deleteBtn">Delete</button></td>
+            </tr>
+        `;
+      }
+
+      function onDeleteRow(e) {
+        if (!e.target.classList.contains("deleteBtn")) {
+          return;
+        }
+
+        const btn = e.target;
+        btn.closest("tr").remove();
+      }
+
+      formEl.addEventListener("submit", onAddtask);
+      tableEl.addEventListener("click", onDeleteRow);
