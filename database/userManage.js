17c2f99da375a7eaf3490644d1fd2f3c9f5637d9
TobiasBrand-GI <79696931+TobiasBrand-GI@users.noreply.github.com>
2021-12-20 12:23:24 +0100

    added User Manager

---
+++ database/userManage.js
@@ -0,0 +1,178 @@
+/**
+ * The User Model from `./schema/userSchema.js` 
+ */
+ const User = require('./schema/userSchema').userModel;
+
+ /**
+  * The User Schema from `./schema/userSchema.js` 
+  */
+ const UserSchema = require('./schema/userSchema').userSchema;
+ 
+ /**
+  * The Task Model from `./schema/taskSchema.js`
+  */
+ const Task = require('./schema/taskSchema.js').taskModel;
+ 
+ /**
+  * The mongoose package from npm.
+  */
+ const mongoose = require('mongoose');
+ 
+ 
+ 
+ /**
+  * Getting all entries inside  the database which are in the collection Users. If there is no entry it returns an empty array. Also return
+  * some metadata 
+  * @returns An object with metadata and the actual data stored in the data field.
+  */
+ async function getAllUsers() {
+     try {
+         let result = await User.find();
+         return {
+             ok: true,
+             message: "Successfull loaded",
+             data: result,
+             dataLength: result.length
+         }
+     } catch (error) {
+         return {
+             ok: false,
+             message: "Error occured",
+             error
+         }
+     }
+ }
+ 
+ /**
+  * Getting the entry inside the databases collection Users with the given Object id. If there is no entry with this id the function will 
+  * return null 
+  * @param {mongoose.Schema.Type.ObjectID} id The id of the searched object.
+  * @returns The user object with the given id, if exists.
+  */
+ async function getOneUser(id) {
+     try {
+         let result = await User.findById(id);
+         return {
+             ok: true,
+             data: result,
+             message: "Successfull loaded"
+         }
+     } catch (error) {
+         return {
+             ok: false,
+             data: null,
+             message: "An error occured",
+             error,
+         }
+     }
+ }
+ 
+ /**
+  * This function create a new User. It will validate if the input parameters are valid. 
+  * The attributes garden, tasks and presenceTime will be initialized but not filled.
+  * @param {{username:String,firstname: String,lastname:String,password:String,email:String, telefonNumber:String}} UserData The data which be wanted to store in the DB.
+  */
+ async function createUser(userData) {
+ 
+     let user = {
+         username: userData.username,
+         firstname: userData.firstname,
+         lastname: userData.lastname,
+         password: userData.password,
+         email: userData.email,
+         telefonNumber: userData.telefonNumber,
+         garden:"",
+         tasks: "",
+         presenceTime: ""
+     }
+     try {
+         await User.create(user);
+     } catch (error) {
+         return {
+             ok: false,
+             data: null,
+             messsage: "Validation error occured",
+             error
+         }
+     }
+ 
+     return {
+         ok: true,
+         data: user,
+         message: "Successfull added"
+     }
+ }
+ 
+ 
+ 
+ /**
+  * This function delete the user with the given id. If the user is not found the function will return null.
+  * @param {mongoose.SchemaType.ObjectID} id The id of the user which should be deleted. 
+  * @returns An object with the status of the delete operation, in the ok field as boolean and a message in the message field. If the request
+  * was successful the data field will hold the return value from mongoose. Else the data field will be null and the error field will hold the error
+  * object.
+  */
+ async function deleteOneUser(id) {
+     try {
+         let result = await User.remove({
+             _id: id
+         });
+         return {
+             ok:true,
+             message: "Successfully removed Task",
+             data: result
+         } 
+     } catch (error) {
+         return {
+             ok:false,
+             message: "An error occured",
+             error
+         } 
+     }
+ }
+ /**
+  * This function takes in the modified user object and updates the user with the given id. The user object must contain the id of the user.
+  * All data from the task object needs to be send with the request. The function will check if the user exists. If the task does not exist the function will return null. 
+  * Also the function return an status as boolean and a message in the message field. If an error occured the data field will be null and the error field will hold the error object.
+  * @param {any} userData The data from req.body which should be updated. Need to contain the id of the user. 
+  * Also must contain username, firstname, lastname, password, email, telefonNumber, garden, tasks, presenceTime.
+  * @returns The updated user object in the result field. If the task does not exist the result field will be null. If an error occured the 
+  * result field will be null and the error field will hold the error object. Also containing a status field as boolean and a message field.
+  */
+ async function modifyUser(userData) {
+     let user = {
+        username: userData.username,
+        firstname: userData.firstname,
+        lastname: userData.lastname,
+        password: userData.password,
+        email: userData.email,
+        telefonNumber: userData.telefonNumber,
+        garden: userData.garden,
+        tasks: userData.tasks,
+        presenceTime: userData.presenceTime
+     }
+     try {
+         let result = await User.findByIdAndUpdate(userData._id, user);
+         return {
+             ok: true,
+             data: result,
+             message: "Successfull updated" 
+         }
+     } catch (error) {
+         return {
+             ok: false,
+             data: null,
+             message: "Validation error occured",
+             error
+         }
+     }
+ } 
+ 
+ 
+ module.exports = {
+     getAllUsers,
+     getOneUser,
+     createUser,
+     deleteOneUser,
+     modifyUser
+ };
--- routes/users.js
+++ routes/users.js
@@ -1,81 +1,46 @@
 var express = require('express');
-const app = express();
 var router = express.Router();
-const assert = require('assert');
+let userManager = require('./../database/userManage');
 
-//Here we are configuring express to use body-parser as middle-ware
-app.use(express.json());
-app.use(express.urlencoded( {extended: true} )); // because Error: 'body-parser deprecated undefined extended'
-
-//MongoConnect
-const url = 'mongodb://localhost:27017' // connection URL
-const dbName = 'easyGardenDB' // database name
-const collectionName = 'users' // collection name
-
-const MongoClient = require('mongodb').MongoClient
-const client = new MongoClient(url) // mongodb client
 
 
 /**
  * Returning all users listed in the database. If there is no entry, it returns an empty JSON object.
  */
 router.get('/', function (req, res, next) {
-    client.connect(function(err) 
-  {
-    assert.strictEqual(null, err);
-  
-    const db = client.db(dbName); //Database
-    const collection = db.collection(collectionName); //Collection
-
-    // Find all documents
-    collection.find({}).toArray(function(err, docs) 
-    {
-      assert.strictEqual(err, null);
-      res.json(docs); //return documents from Database
-    })
-  })
+  let response = await userManager.getAllUsers();
+    response.ok ? res.status(200) : res.status(500);
+    res.json(response);
+    res.send();
 });
+
+/**
+ * Returning the entry in the task database with the given id. If there is no entry with these id, the response will return an error message.
+ */
+ router.get('/:id', async function (req, res, next) {
+  let response = await userManager.getOneUser(req.params.id);
+  response.ok ? res.status(200) : res.status(500);
+  res.json(ressponse);
+  res.send();
+})
 
 /**
  * Create a new user
  * Requirements for an successfull request:
  * {@code {
  *    "username":String,
- *    "email":String,
+ *    "firstname":String,
+ *    "lastname":String,
  *    "password":String,
- *    "phonenumber":String,
+ *    "email":String,
+ *    "telefonNumber":String,
  * }} 
  */
-router.post('/create', (req, res, next) => {
-    let user = {}
-    user.username = "Max Mustermann"
-    user.email = "max@mustermann.de"
-    user.password = "1234567890"
-    user.phonenumber= "01575123456"
-
-    // connect to the mongodb database and afterwards, insert one the new element
-    client.connect(function(err) 
-    {
-      assert.equal(null, err)
-    
-      console.log('Connected successfully to server')
-    
-      const db = client.db(dbName)
-      const collection = db.collection(collectionName)
-
-      // Insert the document in the database
-      collection.insertOne(user, function(err, result) 
-      {
-        assert.equal(err, null)
-        assert.equal(1, result.result.ok)
-        console.log(`Inserted ${result.insertedCount} document into the collection`)
-      })
-      
-    })
-    res.json({
-        'message': true
-    })
-    res.send();
+ router.post('/create', async (req, res, next) => {
+  let response = await userManager.createUser(req.body);
+  response.ok ? res.status(200) : res.status(500);
+  res.json(response);
+  res.send();
 })
 
 /**
@@ -83,90 +48,30 @@ router.post('/create', (req, res, next) => {
  * Requirements for an successfull request:
  * {@code {
  *    "username":String,
- *    "email":String,
+ *    "firstname":String,
+ *    "lastname":String,
  *    "password":String,
- *    "phonenumber":String,
+ *    "email":String,
+ *    "telefonNumber":String,
+ *    "garden": GardenID,
+ *    "tasks": TaskID,
+ *    "presenceTime": DateObject
  * }} 
  */
-router.post('/modify',(req,res,next) =>{
-    client.connect(function(err)
-    {
-      console.log("connected succesful")
-      assert.strictEqual(null, err)
-      const db = client.db(dbName) //database
-      const collection = db.collection(collectionName) //collection
-      collection.find({username: "Max Mustermann"}).toArray(function(err, docs)
-      {
-
-        assert.strictEqual(err, null);
-        // checks if not 0
-        if(docs.length > 0) {
-            //Update the document in the database
-            collection.updateOne({username: "Max Mustermann"}, {$set:{email: "max@maus.de",password:"Helloworld", phonenumber:"987654321"}}, function(err, result)
-            {
-              assert.strictEqual(err, null)
-              assert.strictEqual(1, result.result.ok)
-            })
-            res.json({
-                'message': true
-                })
-                res.send();
-        }
-        else {
-            res.json({
-                'message': false
-                })
-                res.send();
-        }
-      })
-    })
-
-})
-
-module.exports = router;
+ router.post('/modify', async (req, res, next) => {
+  let response = await userManager.modifyUser(req.body);
+  response.ok? res.status(200):res.status(500);
+  res.json(response);
+  res.send();
+});
 
 /**
- * Delete an existing user
- * Requirements for an successfull request:
- * {@code {
- *    "username":String,
- *    "email":String,
- *    "password":String,
- *    "phonenumber":String,
- * }} 
+ * Delete an existing user given by an id
  */
-router.post('/delete', function(req, res, next)
-{
-    client.connect(function(err)
-    {
-        const db = client.db(dbName)
-        const collection = db.collection(collectionName)
-        //check if number exists
-        collection.find({username: "Max Mustermann"}).toArray(function(err, docs)
-        {
-            assert.strictEqual(err, null)
-            // if document exists, delete it
-            if(docs.length >0){
-                assert.strictEqual(null, err);
-
-                //delete Document
-                collection.deleteOne({username: "Max Mustermann"}, function(err, results){
-                    assert.strictEqual(err, null)
-                })
-
-                res.json({
-                'message': true
-                })
-                res.send();
-            }
-            // if something fails, redirect
-            else {
-                res.json({
-                    'message': false
-                })
-                res.send();
-            }
-        })
-    })
-})
+ router.get('/delete/:id', async (req, res, next) => {
+  let response = await userManager.deleteOneUser(req.params.id);
+  response.ok? res.status(200):res.status(500);
+  res.json(response);
+  res.send();
+});
 module.exports = router; //export as router
