9859c4908ea188c24b13e0630ece90f7f4c25fc1
TobiasBrand-GI <tobias.brand1@web.de>
2022-01-24 12:41:43 +0100

    finished task form

---
+++ public/javascript/taskFormFunctions.js
@@ -0,0 +1,63 @@
+function fillUsers(){
+    $.ajax({
+        url:"/users",
+        method: "GET",
+      }).done(function(res){
+        let users=res.data;
+        let select=document.getElementById("assignedTo")
+        for(i=0; i<users.length;i++){
+          var option = document.createElement("option");
+          option.text = users[i].username;
+          option.value=users[i].username;
+          select.add(option);
+        }
+        })
+}
+function fillTaskSubject(value){
+    let subjectSelect=document.getElementById("taskSubject");
+    if(value=="Plant"){
+        $("#taskSubject").empty();
+        $.ajax({
+            url:"/plantManager",
+            method: "GET",
+          }).done(function(res){
+            let plants=res.data;
+            for(i=0; i<plants.length;i++){
+              var option = document.createElement("option");
+              option.text = plants[i].plantName;
+              option.value=plants[i].plantName;
+              subjectSelect.add(option);
+            }
+        })
+    }else if(value=="Object"){
+        $("#taskSubject").empty();
+        $.ajax({
+            url:"/objectManager",
+            method: "GET",
+        }).done(function(res){
+            let objects=res.data;
+            for(i=0; i<objects.length;i++){
+            var option = document.createElement("option");
+            option.text = objects[i].objectName;
+            option.value=objects[i].objectName;
+            subjectSelect.add(option);
+            }
+        })
+    }else if(value=="Process"){
+        $("#taskSubject").empty();
+        $.ajax({
+            url:"/processManager",
+            method: "GET",
+        }).done(function(res){
+            let process=res.data;
+            for(i=0; i<process.length;i++){
+            var option = document.createElement("option");
+            option.text = process[i].name;
+            option.value=process[i].name;
+            subjectSelect.add(option);
+            }
+        })
+    }
+}
+fillUsers();
+fillTaskSubject("Plant");
